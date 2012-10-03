2039c06cbdca2e3a4083e30f5333d9bb89a15ba7
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-03 23:02:35 +0100

    initial commit

---
+++ ActionFilterDemo/ActionFilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilterDemo.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Designer")]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ActionFilterDemo/ActionFilterDemo/Filters/DebugInfoAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace ActionFilterDemo.Filters
+{
+    // example based on http://blog.tallan.com/2011/02/04/global-action-filters-in-asp-net-mvc-3/
+
+    public class DebugInfoAttribute : ActionFilterAttribute
+    {
+        readonly Stopwatch _startWatch = new Stopwatch();
+        private static string _outputFormat = 
+            "<h4>Debug Environment Info</h4><div class=\"debuginfo\"><table><tr><td>Web Server:</td><td>{0}</td></tr><tr><td>Browser:</td><td>{1}</td></tr><tr><td>Controller</td><td>{2}</td></tr><tr><td>Action:</td><td>{3}</td></tr><tr><td>Execution Time(ms):</td><td>{4}</td></tr></table></div>";
+        
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            _startWatch.Reset();
+            _startWatch.Start();
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            _startWatch.Stop();
+            var browser = filterContext.HttpContext.Request.Browser;
+            filterContext.HttpContext.Response.Write(
+                string.Format(_outputFormat,
+                      HttpContext.Current.Request.ServerVariables["SERVER_NAME"],
+                      String.Format("{0} ({1})",browser.Browser,browser.Version),
+                      filterContext.RouteData.Values["controller"],
+                      filterContext.RouteData.Values["action"],
+                      _startWatch.ElapsedMilliseconds)
+                );
+ 
+        }
+    }
+}
